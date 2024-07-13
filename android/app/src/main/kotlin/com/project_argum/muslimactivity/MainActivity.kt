b8b84f6fdb3de87f1fa959338ec66e893ad350c7
Argumelar Pamungkas <argump.12@gmail.com>
2024-07-13 09:58:54 +0700

    first

---
+++ android/app/src/main/kotlin/com/project_argum/muslimactivity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.project_argum.muslimactivity-project.muslim-activity-project.muslim_activity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
